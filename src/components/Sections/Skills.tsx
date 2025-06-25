cbb8d76057ff06fc6f6933f50a57932ea6e823cc
Baadshah12 <musiclover3040@gmail.com>
2025-06-25 14:10:36 +0530

    hidden removed

--- src/components/Sections/Skills.tsx
+++ src/components/Sections/Skills.tsx
@@ -100,7 +100,7 @@ export default function Skills() {
   <motion.div
     key={selectedCategory}
     className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6"
-    initial="hidden"
+    initial={false}
     animate="visible"
     exit="hidden"
     variants={{
